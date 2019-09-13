d6b87301743fb49e8396e078654e1532b4d6dacf
Jane <glazychevaeo@gmail.com>
2019-09-13 18:06:29 -0400

    Update cell

--- Proj_Queue/Assets/Scripts/Cell.cs
+++ Proj_Queue/Assets/Scripts/Cell.cs
@@ -10,15 +10,15 @@ public class Cell
     public List<GameObject> ObjectsOnTheCell { get { return objectsOnTheCell; } set { objectsOnTheCell = value; } }
 
     [SerializeField]
-    Vector2 cellPosition;
-    public Vector2 CellPositon { get { return cellPosition; } set { cellPosition = value; } }
+    Vector2Int cellPosition;
+    public Vector2Int CellPositon { get { return cellPosition; } set { cellPosition = value; } }
 
-    public Cell(Vector2 _cellPosition)
+    public Cell(Vector2Int _cellPosition)
     {
         this.CellPositon = _cellPosition;
     }
 
-    void AddObjectOnTheCell(GameObject _object)
+    void AddObject(GameObject _object)
     {
         ObjectsOnTheCell.Add(_object);
     }
---
+++ Proj_Queue/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "PlayerData", order = 1)]
+public class PlayerData : ScriptableObject
+{
+    [SerializeField]
+    int health;
+    public int Health {get {return health;} set { health = value; } }
+    [SerializeField]
+    Color32 color;
+    public Color32 Color { get { return color; } set { color = value; } }
+}
