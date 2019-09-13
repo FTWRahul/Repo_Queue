3d4eaca0e3b4a389068f3c65e7dae3108b150be2
CubeMD <markelovdp@gmail.com>
2019-09-13 16:19:09 -0400

    Board non mono with board so

---
+++ Proj_Queue/Assets/Scripts/Board.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Board
+{
+    public List<List<Cell>> board;
+
+    public Board(BoardData boardData)
+    {
+        board = new List<List<Cell>>(); 
+
+        for (int i = 0; i < boardData.height; i++)
+        {
+            for (int j = 0; j < boardData.width; j++)
+            {
+                board[i][j] = new Cell();
+            }
+        }
+    }
+}
