d2f102b89b119b1d11871d87b27bbf72e8612c17
Jane <glazychevaeo@gmail.com>
2019-09-13 16:21:05 -0400

    CellScripts

---
+++ Proj_Queue/Assets/Scripts/Cell.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Cell
+{
+    [SerializeField]
+    List<GameObject> objectsOnTheCell = new List<GameObject>();
+
+    public List<GameObject> ObjectsOnTheCell { get { return objectsOnTheCell; } }
+}
---
+++ Proj_Queue/Assets/Scripts/CellMono.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellMono : MonoBehaviour
+{
+    [SerializeField]
+    Cell cellInfo;
+
+    private void Awake()
+    {
+        Cell cellInfo = new Cell();
+    }
+}
