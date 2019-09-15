dab0b82ae0ae6af76c4fa34920ab6a2d219769b9
CubeMD <markelovdp@gmail.com>
2019-09-15 15:32:19 -0400

    pex

---
+++ Proj_Queue/Assets/Scripts/Cell.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    [SerializeField]
+    Vector2Int gridPosition;
+    public Vector2Int CellPositon { get { return gridPosition; } set { gridPosition = value; } }
+}
+
