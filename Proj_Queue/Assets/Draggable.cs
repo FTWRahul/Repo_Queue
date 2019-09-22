a1030b7e124cac2d186ff6a8745b137f7edc63df
Jane <glazychevaeo@gmail.com>
2019-09-22 16:18:55 -0400

    CardTemplate and canvas

---
+++ Proj_Queue/Assets/Draggable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler,IDragHandler, IEndDragHandler
+{
+    private Transform _areaToDrop = null;
+    private CanvasGroup _canvasGroup;
+
+    private void Start()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        _canvasGroup.blocksRaycasts = false;
+
+        _areaToDrop = transform.parent;
+        transform.SetParent(_areaToDrop.parent);
+    }
+    
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _canvasGroup.blocksRaycasts = true;
+        transform.SetParent(_areaToDrop);
+    }
+
+    public void SetDropArea(Transform area)
+    {
+        _areaToDrop = area;
+        transform.rotation = area.transform.rotation;
+    }
+}
---
+++ Proj_Queue/Assets/DropArea.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropArea : MonoBehaviour , IDropHandler , IPointerEnterHandler, IPointerExitHandler
+{
+    void IDropHandler.OnDrop(PointerEventData eventData)
+    {
+        Draggable draggable = eventData.pointerDrag.GetComponent<Draggable>();
+
+        if (draggable != null)
+        {
+            draggable.SetDropArea(transform);
+        }
+    }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerEnter");
+        //TODO: highlight card 
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerExit");
+        //TODO: unhighlight card
+    }
+}
