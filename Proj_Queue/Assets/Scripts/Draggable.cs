2905a0d6ab1a5e9d39439e50e2cd4bde28126529
Jane <glazychevaeo@gmail.com>
2019-09-23 11:40:51 -0400

    update draggable

--- Proj_Queue/Assets/Scripts/Draggable.cs
+++ Proj_Queue/Assets/Scripts/Draggable.cs
@@ -9,6 +9,8 @@ public class Draggable : MonoBehaviour, IBeginDragHandler,IDragHandler, IEndDrag
     private Transform _areaToDrop = null;
     private CanvasGroup _canvasGroup;
 
+    [SerializeField] private GameObject placeHolder;
+
     private void Start()
     {
         _canvasGroup = GetComponent<CanvasGroup>();
@@ -18,6 +20,11 @@ public class Draggable : MonoBehaviour, IBeginDragHandler,IDragHandler, IEndDrag
     {
         _canvasGroup.blocksRaycasts = false;
 
+/*        placeHolder = new GameObject();
+        placeHolder.transform.SetParent(transform.parent);
+        placeHolder.transform.SetSiblingIndex(transform.GetSiblingIndex());
+        */
+
         _areaToDrop = transform.parent;
         transform.SetParent(_areaToDrop.parent);
     }
@@ -25,12 +32,24 @@ public class Draggable : MonoBehaviour, IBeginDragHandler,IDragHandler, IEndDrag
     void IDragHandler.OnDrag(PointerEventData eventData)
     {
         transform.position = eventData.position;
+        
+        /*
+        for (int i = 0; i < _areaToDrop.childCount; i++)
+        {
+            if (transform.position.x < _areaToDrop.GetChild(i).position.x)
+            {
+                placeHolder.transform.SetSiblingIndex(i);
+            }
+        }*/
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         _canvasGroup.blocksRaycasts = true;
         transform.SetParent(_areaToDrop);
+        /*
+        transform.SetSiblingIndex(placeHolder.transform.GetSiblingIndex());
+        */
     }
 
     public void SetDropArea(Transform area)
