9e45df1785a20f72588cb425042568f215427848
Jane <glazychevaeo@gmail.com>
2019-09-25 20:23:40 -0400

    Deck manager

---
+++ Proj_Queue/Assets/Scripts/Deck/DeckManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Static;
+using UnityEngine.Serialization;
+
+public class DeckManager : MonoBehaviour
+{ 
+    public List<Card> originalDeckList = new List<Card>();
+    public Stack<Card> deck = new Stack<Card>();
+    public List<Card> hand = new List<Card>();
+
+    private void Start()
+    {
+        ResetDeck();
+    }
+
+    void ResetDeck()
+    {
+        originalDeckList.Shuffle();
+
+        foreach (Card card in originalDeckList)
+        {
+            deck.Push(card);
+        }
+    }
+
+    void DealCard()
+    {
+        if (deck.Count <= 0)
+        {
+            ResetDeck();
+        }
+        
+        Card newCard = Instantiate(deck.Pop());
+
+        foreach (Card card in hand)
+        {
+            if (card.cardName == newCard.cardName)
+            {
+                return;
+            }
+        }
+        
+        hand.Add(newCard);
+        
+    }
+}
+
---
+++ Proj_Queue/Assets/Scripts/Static/Helper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Static
+{
+    public static class Helper
+    {
+        //Based on Fisher-Yates Shuffle algorithm
+        public static void Shuffle<T>(this List<T> deck)
+        {
+            var r = new System.Random();
+
+            for (int i = deck.Count - 1; i > 0; i++)
+            {
+                int k = r.Next(i + 1);
+                
+                var temp = deck[i];
+                deck[i] = deck[k];
+                deck[k] = temp;
+            }
+        }
+
+        public static void Push<T>(this List<T> deck, Card card)
+        {
+            
+        }
+        
+        public static Card Pop<T>(this List<T> deck)
+        {
+            return null;
+        }
+    }
+}
