f2d8160983e40a0ccff975d44dfcd0bf6cdef6f6
FTWRahul <r.poundarik@gmail.com>
2019-10-04 18:39:35 -0400

    Proj Deleted again

--- Proj_Queue/Assets/Scripts/Pattern/PatternData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PatternData", menuName = "PatternData", order = 1)]
-public class PatternData : ScriptableObject
-{
-    public List<Vector2Int> positions;
-}
